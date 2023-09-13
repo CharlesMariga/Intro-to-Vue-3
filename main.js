c536cf921b99bca9b1821a70374cdb081c27392d
CharlesMariga <charlesmariga37@gmail.com>
2023-09-13 11:12:44 +0300

    feat: event handling

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ const app = Vue.createApp({
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
         { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" }
-      ]
+      ],
+      cart: 0
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
     }
   }
 })
